d4ee2dfa2f72bf86a9ffbba0386b11e180df9f71
Argenis De La Rosa <hennyrosexo@gmail.com>
2019-11-20 17:58:34 -0500

    Initial Commit

---
+++ Food BrainTests/Food_BrainTests.swift
@@ -0,0 +1,34 @@
+//
+//  Food_BrainTests.swift
+//  Food BrainTests
+//
+//  Created by argenis delarosa on 11/20/19.
+//  Copyright © 2019 argenis delarosa. All rights reserved.
+//
+
+import XCTest
+@testable import Food_Brain
+
+class Food_BrainTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
